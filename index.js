0d3bd2d001dd0743335f4d2f02417d496bb36312
Ivaylo Hristov <ivaylo.hristov@fourth.com>
2018-07-06 11:10:35 +0300

    Adding components

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import React from 'react';
-import ReactNative from 'react-native';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create the main component
+const App = () => {
+    return (
+     <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+     </View> 
+ );
+};
+
+AppRegistry.registerComponent('albums', () => App);
